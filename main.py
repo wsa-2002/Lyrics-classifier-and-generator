ff25dcf304b50c1eea8de418525cc5c3f249ea65
wsa <bensonwang2001@gmail.com>
2022-12-12 15:50:55 +0800

    remove useless dependency

--- main.py
+++ main.py
@@ -30,37 +30,11 @@ async def app_startup():
     from persistence.database import pool_handler
     await pool_handler.initialize(db_config=db_config)
 
-    # if s3 needed
-    from config import s3_config
-    from persistence.s3 import s3_handler
-    await s3_handler.initialize(s3_config=s3_config)
-
-    # if amqp needed
-    from config import amqp_config
-    from persistence.amqp_publisher import amqp_publish_handler
-    await amqp_publish_handler.initialize(amqp_config=amqp_config)
-
-    from persistence.amqp_consumer import make_consumer
-    import processor.amqp
-    report_consumer = make_consumer(amqp_config=amqp_config,
-                                    consume_function=processor.amqp.save_report)
-
-    import asyncio
-    asyncio.ensure_future(report_consumer(asyncio.get_event_loop()))
-
 
 @app.on_event('shutdown')
 async def app_shutdown():
     from persistence.database import pool_handler
     await pool_handler.close()
-
-    # if s3 needed
-    from persistence.s3 import s3_handler
-    await s3_handler.close()
-
-    # if amqp needed
-    from persistence.amqp_publisher import amqp_publish_handler
-    await amqp_publish_handler.close()
 
 
 import middleware.auth
